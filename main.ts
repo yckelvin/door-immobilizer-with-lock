dd957468d3fbe4318b757fa92ecc39cf01d4f348
Kelvin Lee <yckelvin@users.noreply.github.com>
2022-06-24 00:38:18 +0800

    Add unlock-forever function

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    unlock_forever = 1
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        . . . . .
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.AB, function () {
     lock = 0
     basic.showLeds(`
@@ -10,25 +22,34 @@ input.onButtonPressed(Button.AB, function () {
     basic.pause(500)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    unlock_forever = 0
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+let unlock_forever = 0
 let lock = 0
 let force = 0
 lock = 1
+unlock_forever = 0
 basic.forever(function () {
     force = input.magneticForce(Dimension.Strength)
     if (force < 500) {
-        if (lock == 1) {
-            while (force < 500) {
-                force = input.magneticForce(Dimension.Strength)
-                basic.showIcon(IconNames.No)
-                basic.pause(500)
-                basic.clearScreen()
-                basic.pause(500)
-            }
-        } else {
-            while (force < 500) {
-                force = input.magneticForce(Dimension.Strength)
-            }
+        if (lock == 1 && unlock_forever == 0) {
+            force = input.magneticForce(Dimension.Strength)
+            basic.showIcon(IconNames.No)
+            basic.pause(500)
+            basic.clearScreen()
+            basic.pause(500)
         }
+    } else {
         lock = 1
     }
 })
